8335f5f17a0c336c307bcaedd700ddf0e62c9354
Wes Nelson <WesNelson10@outlook.com>
2021-04-10 18:21:52 -0500

    Initial Commit

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -10,6 +10,33 @@ namespace GuessANumber
     {
         static void Main(string[] args)
         {
+            Console.Title = "Guess a Number!";
+            Console.SetWindowSize(40, 40);
+            Random r = new Random();
+            int winningNum = r.Next(0, 100);
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number between 0 and 100: ");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winningNum)
+                {
+                    Console.WriteLine("Too high! Guess Lower.");                   
+                }
+                else if (i < winningNum)
+                {
+                    Console.WriteLine("Too low... Guess higher!");
+                }
+                else if (i == winningNum)
+                {
+                    Console.WriteLine("YOU WIN!");
+                    win = true;
+                }
+            } while (!win);
         }
     }
 }
